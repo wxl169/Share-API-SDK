abfe08ee5f07da68b72b60523c76291f9d826dec
wxl <1695607239@qq.com>
2023-11-09 16:11:25 +0800

    完成用户与接口信息关系的管理

---
+++ src/main/java/org/wxl/shareapiclientsdk/model/User.java
@@ -0,0 +1,13 @@
+package org.wxl.shareapiclientsdk.model;
+
+import lombok.Data;
+
+/**
+ * 用户
+ * @author 16956
+ */
+@Data
+public class User {
+
+    private String username;
+}
---
+++ src/main/java/org/wxl/shareapiclientsdk/utils/SignUtils.java
@@ -0,0 +1,23 @@
+package org.wxl.shareapiclientsdk.utils;
+
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.Digester;
+
+/**
+ * 签名工具
+ * @author 16956
+
+ */
+public class SignUtils {
+    /**
+     * 生成签名
+     * @param body
+     * @param secretKey
+     * @return
+     */
+    public static String genSign(String body, String secretKey) {
+        Digester md5 = new Digester(DigestAlgorithm.SHA256);
+        String content = body + "." + secretKey;
+        return md5.digestHex(content);
+    }
+}
