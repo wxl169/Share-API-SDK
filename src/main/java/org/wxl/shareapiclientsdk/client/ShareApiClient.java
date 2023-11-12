5926f1006d0b8ea799f4fecf235c39e287471567
wxl <1695607239@qq.com>
2023-11-12 10:33:58 +0800

    完成用户与接口信息关系的管理

--- src/main/java/org/wxl/shareapiclientsdk/client/ShareApiClient.java
+++ src/main/java/org/wxl/shareapiclientsdk/client/ShareApiClient.java
@@ -27,25 +27,42 @@ public class ShareApiClient {
         this.secretKey = secretKey;
     }
 
+    /**
+     * 网关端口
+     */
     private static final String GATEWAY_HOST = "http://localhost:8090";
-    public String getNameByGet(String name) {
-        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("name", name);
-        String result = HttpUtil.get(GATEWAY_HOST + "/api/name/", paramMap);
-        System.out.println(result);
-        return result;
-    }
 
-    public String getNameByPost(String name) {
-        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("name", name);
-        String result = HttpUtil.post(GATEWAY_HOST + "/api/name/post", paramMap);
-        System.out.println(result);
-        return result;
-    }
 
+
+//    public String getNameByGet(String name) {
+//        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+//        HashMap<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("name", name);
+//        String result = HttpUtil.get(GATEWAY_HOST + "/api/name/", paramMap);
+//        System.out.println(result);
+//        return result;
+//    }
+//
+//    public String getNameByPost(String name) {
+//        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+//        HashMap<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("name", name);
+//        String result = HttpUtil.post(GATEWAY_HOST + "/api/name/post", paramMap);
+//        System.out.println(result);
+//        return result;
+//    }
+//
+//    public String getUsernameByPost( User user) {
+//        String json = JSONUtil.toJsonStr(user);
+//        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
+//                .addHeaders(getHeaderMap(json))
+//                .body(json)
+//                .execute();
+//        System.out.println(httpResponse.getStatus());
+//        String result = httpResponse.body();
+//        System.out.println(result);
+//        return result;
+//    }
     private Map<String, String> getHeaderMap(String body) {
         Map<String, String> hashMap = new HashMap<>();
         hashMap.put("accessKey", accessKey);
@@ -58,15 +75,14 @@ public class ShareApiClient {
         return hashMap;
     }
 
-    public String getUsernameByPost( User user) {
-        String json = JSONUtil.toJsonStr(user);
-        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
-                .addHeaders(getHeaderMap(json))
-                .body(json)
+    public String onlineInvoke(String parameters, String url) {
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + url)
+                .addHeaders(getHeaderMap(parameters))
+                .body(parameters)
                 .execute();
         System.out.println(httpResponse.getStatus());
         String result = httpResponse.body();
-        System.out.println(result);
         return result;
     }
+
 }
